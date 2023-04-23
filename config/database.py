539ea4b3d0c0abdb36736fe34f3657be80761fba
Lakshya Sharma <lakshya7878@gmail.com>
2023-04-23 22:47:56 +0530

    done

--- config/database.py
+++ config/database.py
@@ -2,4 +2,6 @@ from pymongo import MongoClient
 from pymongo.server_api import ServerApi
 
 uri = "mongodb+srv://sharmalakshya341:sharmalakshya@cluster0.ftb81fm.mongodb.net/info?retryWrites=true&w=majority"
+
+
 conn = MongoClient(uri)
