023ded7608a2ceb880345b6eadb41417031e5542
Lakshya Sharma <lakshya7878@gmail.com>
2023-04-24 00:53:12 +0530

    done

--- config/database.py
+++ config/database.py
@@ -3,7 +3,7 @@ from pymongo.server_api import ServerApi
 import os
 username = os.environ.get('user')
 password = os.environ.get('pass')
-uri = "mongodb+srv://{username}:{password}@cluster0.ftb81fm.mongodb.net/info?retryWrites=true&w=majority"
+uri = "mongodb+srv://sharmalakshya341:sharmalakshya@cluster0.ftb81fm.mongodb.net/info?retryWrites=true&w=majority"
 
 
 conn = MongoClient(uri)
