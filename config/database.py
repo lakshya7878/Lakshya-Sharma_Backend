c65ff0e5a66c657e4e8d0eeeac9a61dc265eab2b
Lakshya Sharma <lakshya7878@gmail.com>
2023-04-24 00:58:49 +0530

    firdone

--- config/database.py
+++ config/database.py
@@ -3,6 +3,8 @@ from pymongo.server_api import ServerApi
 import os
 username = os.environ.get('user')
 password = os.environ.get('pass')
+print(username)
+print(password)
 uri = "mongodb+srv://sharmalakshya341:sharmalakshya@cluster0.ftb81fm.mongodb.net/info?retryWrites=true&w=majority"
 
 
