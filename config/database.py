07bb952f49f3c1979e52284422b6bb749c363575
Lakshya Sharma <lakshya7878@gmail.com>
2023-04-24 01:08:16 +0530

    keys added

--- config/database.py
+++ config/database.py
@@ -3,9 +3,7 @@ from pymongo.server_api import ServerApi
 import os
 username = os.environ.get('user')
 password = os.environ.get('pass')
-print(username)
-print(password)
-uri = "mongodb+srv://sharmalakshya341:sharmalakshya@cluster0.ftb81fm.mongodb.net/info?retryWrites=true&w=majority"
+uri = f'mongodb+srv://{username}:{password}@cluster0.ftb81fm.mongodb.net/info?retryWrites=true&w=majorityf'
 
 
 conn = MongoClient(uri)
